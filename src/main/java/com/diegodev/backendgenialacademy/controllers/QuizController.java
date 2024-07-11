0c39df9a79e9b4df4d56653fe5e7dd054467d617
diegodev-11 <dalvarez@bitz.pe>
2024-07-11 01:01:31 -0500

    added cors permission on quiz controller

--- src/main/java/com/diegodev/backendgenialacademy/controllers/QuizController.java
+++ src/main/java/com/diegodev/backendgenialacademy/controllers/QuizController.java
@@ -2,15 +2,13 @@ package com.diegodev.backendgenialacademy.controllers;
 
 import com.diegodev.backendgenialacademy.dtos.responses.QuizRes;
 import com.diegodev.backendgenialacademy.services.QuizService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/quizzes")
+@CrossOrigin
 public class QuizController {
     private final QuizService quizService;
 
